b54139c64c8adb05313a7e94a773a85fc2fa68fb
Abhikkumar619 <abisheky194@gmail.com>
2024-01-30 08:11:58 +0545

    utils file is added

---
+++ src/Credit_card_project/utils/common.py
@@ -0,0 +1,52 @@
+import yaml
+import os
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from src.Credit_card_project import logger
+import json
+import joblib
+
+@ensure_annotations
+def read_yaml(yaml_path: Path)->ConfigBox: 
+    with open(yaml_path) as yaml_file: 
+        content=yaml.safe_load(yaml_file)
+    logger.info(f"Yaml file read {yaml_path} successfully") 
+        
+    return ConfigBox(content)
+
+@ensure_annotations
+def create_directories(dir_path: list):
+    for path in dir_path: 
+        os.makedirs(path, exist_ok=True) 
+        logger.info(f"Directories created {dir_path}")
+        
+@ensure_annotations
+def save_json(data: dict, path: Path): 
+    with  open(path, 'w') as json_file: 
+        json.dump(data, json_file)
+    logger.info(f"Json data save at {path} sucessful")
+    
+    
+@ensure_annotations
+def load_json(path: Path): 
+    with open(path) as json_path:
+        content=json.load(json_path)
+        
+    logger.info(f"Json data loaded from path {path}")
+    
+@ensure_annotations
+def save_binaryFile(path: Path, data: any): 
+    joblib.dump(data,path )
+    logger.info(f"Binary file saved at {path}")
+
+@ensure_annotations
+def load_binaryFile(path: Path): 
+    obj=joblib.load(path)
+    logger.info(f"Binary file loaded sucessfully from path {path}")
+    return obj
+    
+    
+        
+        
+    
